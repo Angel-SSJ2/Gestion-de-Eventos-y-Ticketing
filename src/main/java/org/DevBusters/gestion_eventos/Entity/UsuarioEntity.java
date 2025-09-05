006ee42a6ee769adaa55ec74cf44f41174495f1d
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-04 22:50:45 -0600

    Se agrego la clase Entity de Usuarios

--- src/main/java/org/DevBusters/gestion_eventos/Entity/UsuarioEntity.java
+++ src/main/java/org/DevBusters/gestion_eventos/Entity/UsuarioEntity.java
@@ -1,4 +1,27 @@
 package org.DevBusters.gestion_eventos.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+
+    private String nombreUsuario;
+
+    private String correo;
+
+    private String contrasena;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<TicketEntity> tickets;
 }
