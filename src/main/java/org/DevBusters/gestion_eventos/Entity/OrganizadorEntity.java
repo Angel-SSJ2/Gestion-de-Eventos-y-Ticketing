4a3559b4ed5a51a8e2086905838e487ea1ba1e9f
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-04 22:49:12 -0600

    Se agrego la clase Entity de organizador

--- src/main/java/org/DevBusters/gestion_eventos/Entity/OrganizadorEntity.java
+++ src/main/java/org/DevBusters/gestion_eventos/Entity/OrganizadorEntity.java
@@ -1,4 +1,27 @@
 package org.DevBusters.gestion_eventos.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Organizadores")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrganizadorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idOrganizador;
+
+    private String nombreOrganizador;
+
+    private String correo;
+
+    private String contrasena;
+
+    @OneToMany(mappedBy = "organizador")
+    private List<EventoEntity> eventos;
 }
