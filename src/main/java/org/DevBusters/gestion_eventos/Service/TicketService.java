45308a3089e5de49b080ea9d1d750a56823532ee
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-04 23:01:14 -0600

    Se agrego la clase service de ticket

--- src/main/java/org/DevBusters/gestion_eventos/Service/TicketService.java
+++ src/main/java/org/DevBusters/gestion_eventos/Service/TicketService.java
@@ -1,4 +1,22 @@
 package org.DevBusters.gestion_eventos.Service;
 
-public class TicketService {
+import org.DevBusters.gestion_eventos.Entity.TicketEntity;
+import org.DevBusters.gestion_eventos.Repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@Service
+public class TicketService implements ITicketService {
+
+    @Autowired
+    private TicketRepository ticketrepository;
+
+    @Override
+    public List<TicketEntity> listarTicket() {
+        List<TicketEntity> tickets = ticketrepository.findAll();
+        return tickets;
+    }
 }
+
