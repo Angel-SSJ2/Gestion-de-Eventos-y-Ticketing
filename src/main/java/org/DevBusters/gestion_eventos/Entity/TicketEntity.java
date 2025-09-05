ff1c8094260d6e3b3aa3db21766e46dd0e0212e1
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-04 22:50:02 -0600

    Se agrego la clase Entity de Tickets

--- src/main/java/org/DevBusters/gestion_eventos/Entity/TicketEntity.java
+++ src/main/java/org/DevBusters/gestion_eventos/Entity/TicketEntity.java
@@ -1,4 +1,34 @@
 package org.DevBusters.gestion_eventos.Entity;
 
-public class Ticketentity {
+import jakarta.persistence.*;
+import lombok.*;
+import org.DevBusters.gestion_eventos.Enum;
+
+@Entity
+@Table(name = "Tickets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idTickets;
+
+    @ManyToOne
+    @JoinColumn(name = "idEvento", nullable = false)
+    private EventoEntity evento;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario", nullable = false)
+    private UsuarioEntity usuario;
+
+    private String codigoQr;
+
+    private Double precio;
+
+    @Enumerated(EnumType.STRING)
+    private Enum estado;
+
+
 }
