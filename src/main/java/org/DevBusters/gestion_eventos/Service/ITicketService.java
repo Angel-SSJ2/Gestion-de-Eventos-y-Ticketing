d874c5c9cb40001232f0f897c796fff0533ea0c4
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-04 22:59:44 -0600

    Se agrego la clase interfas de tickets

--- src/main/java/org/DevBusters/gestion_eventos/Service/ITicketService.java
+++ src/main/java/org/DevBusters/gestion_eventos/Service/ITicketService.java
@@ -1,4 +1,10 @@
 package org.DevBusters.gestion_eventos.Service;
 
+import org.DevBusters.gestion_eventos.Entity.TicketEntity;
+
+import java.util.List;
+
 public interface ITicketService {
+
+    public List<TicketEntity> listarTicket();
 }
